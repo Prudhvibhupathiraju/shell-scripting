d8d5abc7df9846687fb26240c0116403a4ca60c3
Prudhvibhupathiraju <147089185+Prudhvibhupathiraju@users.noreply.github.com>
2026-03-13 05:49:13 +0000

    c1e44

--- helloworld.sh
+++ helloworld.sh
@@ -19,10 +19,10 @@ echo "All the arguments passed :: $@"
 echo "script-name :: $0"
 user=$(whoami)
 echo "User logged in is $user"
-friuts=("Apple" "Kiwi" "Mango")
-echo "Fruit 1 is ${friuts[0]}"
-echo "Fruit 2 is ${friuts[1]}"
-echo "Fruit 3 is ${friuts[2]}"
+fruits=("Apple" "Kiwi" "Mango")
+echo "Fruit 1 is ${fruits[0]}"
+echo "Fruit 2 is ${fruits[1]}"
+echo "Fruit 3 is ${fruits[2]}"
 if [ $age2 -ge 2 ]
 then
 echo "$Person2 ate ${fruits[0]}"

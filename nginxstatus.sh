46127b96e1e97ef900ee88db8fb5072ace223201
Prudhvibhupathiraju <147089185+Prudhvibhupathiraju@users.noreply.github.com>
2026-03-17 08:03:50 +0000

    chjajs

--- nginxstatus.sh
+++ nginxstatus.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 service="nginx"
 email="prudhvi.bhupathiraju999@gmail.com"
+host=$(hostname)
+DATE=$(date "+%Y-%m-%d %H:%M:%S")
+
 systemctl status $service | grep "running"
 if [ $? -ne 0 ]
 then
-echo "$service is down on $(hostname)" | mail -s "$service down alert" $email
+LOGS=$(journalctl -u nginx -n 10 --no-pager)
+MESSAGE=$(cat <<EOF
+=============================
+🚨 SERVICE ALERT 🚨
+=============================
+
+Server      : $HOST
+Service     : $SERVICE
+Status      : DOWN ❌
+Time        : $DATE
+
+Recent Logs
+-----------------------------
+$LOGS
+
+Action Required:
+systemctl status $SERVICE
+
+=============================
+Linux Monitoring System
+=============================
+EOF
+)
+echo "$message" | mail -s "🚨 ALERT: $SERVICE DOWN on $HOST" $email
 else
 echo "$service is up on $(hostname)" | mail -s "$service is running" $email
 fi
